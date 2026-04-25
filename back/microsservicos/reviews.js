36a7aabefd6ca3fdc99034b7ad55c031638f986e
veloy25 <veloy.devmaua@gmail.com>
2026-04-25 08:43:32 -0300

    refactor(index.js): - extrai a lógica da entidade depoimentos do index.js para um arquivo próprio -> reviews.js; - Cria a tabela de depoimentos caso não exista; - Estabelece o modelo de negócio da criação e obtenção dos depoimentos.

---
+++ back/microsservicos/reviews.js
@@ -0,0 +1,47 @@
+import mysql from "mysql2";
+import express from "express"
+
+
+const app = express();
+
+class reviews{
+    constructor (dbPool){
+        this.pool = dbPool
+        this.initializeDatabase()
+    }
+
+    async initializeTable(){
+        await this.pool.query(`CREATE TABLE IF NOT EXISTS depoimentos (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(100) NOT NULL,
+            email VARCHAR(180) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+          )`);
+        console.log("Database table 'users' is ready.");
+    }
+
+    async getReview(){
+        const [rows] = await this.pool.query("SELECT * FROM depoimentos ORDER by criado_em")
+
+        return rows
+    }
+
+    async createReview(nomeCachorro, nomeTutor, raca, comentario){
+        if (!nomeCachorro || !nomeTutor || !raca){
+            return [null, {error: "É necessário preencher todos os campos"}]
+        }
+
+        const [result] = await this.pool.query("INSERT INTO depoimentos (nomeCachorro, nomeTutor, raca, comentario) VALUES (?, ?, ?, ?)",
+        [nomeCachorro, nomeTutor, raca, comentario]);
+
+        return [true, {
+            nomeCachorro: nomeCachorro,
+            nomeTutor: nomeTutor,
+            raca: raca,
+            comentario: comentario,
+            criado_em: new Date().toISOString(),
+        }]
+    }
+
+}
